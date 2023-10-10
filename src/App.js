559a1f1aab1440430b892e08a10aa5a757f5f5ec
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-10 15:25:07 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const tasks = [
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 2,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 6,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 31,
+    "id": 4,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": true
+  },
+  {
+    "userId": 12,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 85,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  }
+];
+
 function App() {
+  const [taskStatus, setTaskStatus] = useState({});
+
+  function handleTaskStatusChange(taskId, completed) {
+    setTaskStatus((prevStatus) => ({
+      ...prevStatus,
+      [taskId]: completed,
+    }));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Uzdevumu saraksts</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <label>
+              <input
+                type="radio"
+                name={`task-${task.id}`}
+                value="completed"
+                checked={taskStatus[task.id] === true}
+                onChange={() => handleTaskStatusChange(task.id, true)}
+              />
+              Pabeigts
+            </label>
+            <label>
+              <input
+                type="radio"
+                name={`task-${task.id}`}
+                value="not-completed"
+                checked={taskStatus[task.id] === false}
+                onChange={() => handleTaskStatusChange(task.id, false)}
+              />
+              Nav pabeigts
+            </label>
+            {task.title}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
